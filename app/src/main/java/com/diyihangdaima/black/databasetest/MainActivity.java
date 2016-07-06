df20238b7c8d5c1b0c7538909ca4351e2eb3a06b
blackpig <blackpig5210@gmail.com>
2016-07-06 19:27:48 +0800

    frirst commit

---
+++ app/src/main/java/com/diyihangdaima/black/databasetest/MainActivity.java
@@ -0,0 +1,26 @@
+package com.diyihangdaima.black.databasetest;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+    private Button createDatabase;
+    private MyDatabaseHelper dbHelper;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        dbHelper = new MyDatabaseHelper(this, "BookStore.db", null, 1);
+        createDatabase = (Button) findViewById(R.id.creata_database);
+        createDatabase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dbHelper.getWritableDatabase();
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/diyihangdaima/black/databasetest/MyDatabaseHelper.java
@@ -0,0 +1,37 @@
+package com.diyihangdaima.black.databasetest;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
+
+/**
+ * Created by black on 2016/7/2/0002.
+ */
+public class MyDatabaseHelper extends SQLiteOpenHelper {
+    public static final String CREATE_BOOK = "create table Book("
+    + "id integer primary key autoincrement,"
+    + "author text,"
+    + "price real,"
+    + "pages integer,"
+    + "name text)";
+
+    private Context mContext;
+
+    public MyDatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory,
+                            int version) {
+        super(context, name, factory, version);
+        mContext = context;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_BOOK);
+        Toast.makeText(mContext, "Create succeeded", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
